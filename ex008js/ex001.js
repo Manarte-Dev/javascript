b0df296031f57232625d649970c3a6d1f42ad110
Manarte-Dev <220400922+Manarte-Dev@users.noreply.github.com>
2025-07-25 17:02:19 -0300

    0

---
+++ ex008js/ex001.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A velociade do seu veiculo é ${vel}`)
+if( vel > 60 ){
+    console.log(`Voce foi a multado a velocidade permitida é 60 voce está a ${vel}`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
---
+++ ex008js/ex002.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+console.log(`Neste pais ${pais}`)
+if(pais == 'Brasil'){
+    console.log('Voce ta lascado cumpanheiro kk')
+}
+else{
+    console.log('Voce tem sorte é estrangeiro')
+}
